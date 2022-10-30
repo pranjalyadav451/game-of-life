a564a2129322410ad31b309376e5e8a6e5f97264
Pranjal Yadav <pranjal@pop-os.localdomain>
2022-10-30 21:48:50 +0530

    first commit

---
+++ src/Components/GridItem/GridItem.js
@@ -0,0 +1,5 @@
+import "./GridItem.css";
+const GridItem = () => {
+	return <div className="grid-item"></div>;
+};
+export default GridItem;
---
+++ src/Components/GridWrapper/GridWrapper.js
@@ -0,0 +1,17 @@
+import GridItem from "../GridItem/GridItem";
+import "./GridWrapper.css";
+const GridWrapper = () => {
+	return (
+		<div className="grid-wrapper">
+			<GridItem />
+			<GridItem />
+			<GridItem />
+			<GridItem />
+			<GridItem />
+			<GridItem />
+			<GridItem />
+			<GridItem />
+		</div>
+	);
+};
+export default GridWrapper;
